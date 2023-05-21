38e28e1139d7fa8f4d041b4778be4ca30f3185fd
NdekoCode <arickbulakali@gmail.com>
2023-05-21 11:47:23 +0200

    add list and iteration hotel data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HotelListComponent } from './components/hotel-list/hotel-list.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, HotelListComponent],
+	imports: [BrowserModule, AppRoutingModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/libs/models/Hotel.model.ts
@@ -0,0 +1,8 @@
+export class Hotel {
+	hotelId!: number;
+	hotelName!: string;
+	description!: string;
+	price!: number;
+	imageUrl!: string;
+	rating!: number;
+}
